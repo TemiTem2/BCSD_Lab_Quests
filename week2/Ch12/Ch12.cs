b13864c252257e7f588f98496b34bff23a7b15ae
Mozin <temiumaru@naver.com>
2025-03-19 18:53:26 +0900

    #12
    업스캐일링
    is연산자 : 객체가 해당 형식에 해당하는지 검사하고 결과를 bool값으로 반환
    as 연산자:형식 변환 연산자와 같은 역할로 형변환이 가능하면 형변환 후 인스턴스 반환, 실패하면 null반환

--- week2/Ch12/Ch12.cs
+++ week2/Ch12/Ch12.cs
@@ -4,88 +4,37 @@ using UnityEngine;
 
 public class Ch12 : MonoBehaviour
 {
+    private Slime slime;
+    private Goblin goblin;
     private void Awake()
     {
-        /*Player player = new Player();
-        player.SetCurrentHP(100);
-        Debug.Log($"HP:{player.GetCurrentHP()}");
-        */
-        
+        slime = new Slime();
+        goblin = new Goblin();
+        slime.Attack();
+        goblin.Attack();
     }
 }
-
-/*public class Player
-{
-    private int CurrentHP;
-
-    public void SetCurrentHP(int hp)//캡슐화:정보은닉으로 인해 접근할수 없는 정보를 메소드를 사용해 수정,접근하게 함
-    {
-        CurrentHP = hp;
-    }
-    public int GetCurrentHP() 
-    {  
-        return CurrentHP; 
-    
-    }
-}
-*/
-/*public class Entity
-{
-    public string ID;
-    protected int currentHP;
-
-    private void Initialize()
-    {
-        ID = "Noname";
-    }
-
-    public void RecoveryHP(int hp)
-    {
-        currentHP += hp;
-    }
-}
-
-public class Player : Entity //상속:부모 클래스에서의 public, protected로 정의된 변수와 메소드를 자식 클래스에서 정의한것처럼 사용 가능
-{
-
-    private string ID;
-    public Player(string id, int hp)
-    {
-        base.ID = id;   //같은 이름의 변수나 메소드가 부모 자식 클래스에 둘다 존재할때, base 키워드 = 부모 클래스/this키워드 = 자기 클래스의 변수나 메소드에 접근
-        ID = id;
-        currentHP = hp;
-
-        RecoveryHP(1000);
-    }
-}*/
 public class Entitiy
 {
-    public void Attack()
+    public virtual void Attack()
     {
-        Debug.Log("적을 공격한다");
-    }
-    public void Heal()
-    {
-        Debug.Log("체력을 회복한다");
+        Debug.Log("적을 공격한다"); //메소드 오버라이딩 부모클래스에서 virtual로 메소드 정의
     }
 }
 
 public class Goblin : Entitiy
 {
-    public void Avoid()
+    public override void Attack() //자식 클래스에서 override로 메소드를 다시 정의(private로 정의된 메소드는 오버라이딩 불가)
     {
-        Debug.Log("도망가자!");
+        Debug.Log("고블린 공격!");
     }
 
-    public void Heal()
-    {
-        Debug.Log("고블린 쳬력증가");
-    }
 }
-//private Entitiy entity = new Entitiy();//사용가는맴버 : Attack(), Heal()
+public class Slime : Entitiy
+{
+    public override void Attack()
+    {
+        Debug.Log("슬라임 공격");
+    }
 
-//private Goblin Goblin = new Goblin();//사용가는맴버 : Attack(), Avoid(), Heal() <-Goblin에 정의된 Heal()
-//자식 클래스 참조 변수 + 자식 클래스 개채 -> 자식클래스 메소드 출력
-
-//private Entitiy entity = new Goblin();//사용가는맴버 : Attack(), Heal() <-Entity에 정의된 Heal()
-//부모 클래스 참조 변수 + 자식 클래스 개채 -> 부모 클래스 메소드 출력
+}
