800348c966cf2b0dd44b339fb1210d8f148ae417
Mozin <temiumaru@naver.com>
2025-03-19 19:16:42 +0900

    인터페이스
    추상 클래스에서 추상적인 부분(추상 메소드. 프로퍼터)만 따로 떼어 구현한것
    
    추상 클래스와 달리 상속 가능
    
    이때 구현되고 정의되는 접근 지정자는 public이다.
    
    표준화가 가능하고 여러 클래스들을 관계 맺을수있다는  장점이 있다

--- week2/Ch12/Ch12.cs
+++ week2/Ch12/Ch12.cs
@@ -4,42 +4,25 @@ using UnityEngine;
 
 public class Ch12 : MonoBehaviour
 {
-    private Slime slime;
-    private Goblin goblin;
+    Goblin goblin;
+    
     private void Awake()
     {
-        slime = new Slime();
-        goblin = new Goblin();
-        slime.Attack();
-        goblin.Attack();
-    }
-}
-public abstract class Entitiy //추상화:자식 클래스에서 구현시켜야 하는 메소드를 만드는것
-{
-    protected int damage;
-    protected int currentHP;
-    public abstract void Attack(Entitiy target); //추상 메소드:호출시 하는 행동을 자식클래스에서 정의해야함->강제성
-    public void TakeDamage(int damage)
-    {
-        if (currentHP > damage) 
-        {
-            currentHP -= damage; 
-        }
-        else { Debug.Log("DIE"); }
+        Vector3 position = new Vector3(3f, 4f, 3f);
+        goblin.MoveTo(position);
     }
 }
 
-public class Goblin : Entitiy
+interface IMovingEntity
 {
-    public Goblin(int damage, int hp)
-    {
-        base.damage = damage;
-        currentHP = hp;
-    }
-    public override void Attack(Entitiy target) //자식 클래스에서 override로 추상 메소드를 다시 정의
-    {
-        Debug.Log("고블린 공격!");
-        target.TakeDamage(damage);
+    void MoveTo(Vector3 destination);
+}
+
+public class Goblin : IMovingEntity
+{
+    
+    public void MoveTo(Vector3 destination) {
+        Debug.Log($"{destination}까지 이동");
     }
 
 }
