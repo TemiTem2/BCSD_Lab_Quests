95d08f1ffa671cc9d8c07c82340951d39a545234
Mozin <temiumaru@naver.com>
2025-03-19 18:31:09 +0900

    #12
    상속 추가

--- week2/Ch12/Ch12.cs
+++ week2/Ch12/Ch12.cs
@@ -6,17 +6,18 @@ public class Ch12 : MonoBehaviour
 {
     private void Awake()
     {
-        Player player = new Player();
+        /*Player player = new Player();
         player.SetCurrentHP(100);
         Debug.Log($"HP:{player.GetCurrentHP()}");
+        */
     }
 }
 
-public class Player
+/*public class Player
 {
     private int CurrentHP;
 
-    public void SetCurrentHP(int hp)
+    public void SetCurrentHP(int hp)//캡슐화:정보은닉으로 인해 접근할수 없는 정보를 메소드를 사용해 수정,접근하게 함
     {
         CurrentHP = hp;
     }
@@ -24,5 +25,35 @@ public class Player
     {  
         return CurrentHP; 
     
+    }
+}
+*/
+public class Entity
+{
+    public string ID;
+    protected int currentHP;
+
+    private void Initialize()
+    {
+        ID = "Noname";
+    }
+
+    public void RecoveryHP(int hp)
+    {
+        currentHP += hp;
+    }
+}
+
+public class Player : Entity //상속:부모 클래스에서의 public, protected로 정의된 변수와 메소드를 자식 클래스에서 정의한것처럼 사용 가능
+{
+
+    private string ID;
+    public Player(string id, int hp)
+    {
+        base.ID = id;   //같은 이름의 변수나 메소드가 부모 자식 클래스에 둘다 존재할때, base 키워드 = 부모 클래스/this키워드 = 자기 클래스의 변수나 메소드에 접근
+        ID = id;
+        currentHP = hp;
+
+        RecoveryHP(1000);
     }
 }
